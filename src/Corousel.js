fe939517efd424f6335440894d19c74a4c839b28
hariom-sinha58 <sinhahariom2@gmail.com>
2021-05-08 10:36:33 +0530

    second commit

--- src/Corousel.js
+++ src/Corousel.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './Corousel.scss';
 
+{/*This Functional Component is used for rendering the Slide Indicators based on the props passed.*/}
 const CorouselNav = ({imgs})=>{
   return(
     <ol className="carousel-indicators">
@@ -13,13 +14,15 @@ const CorouselNav = ({imgs})=>{
   )
 }
 
+{/* This is a Functional Component which i I have used in the CorouselWrapper Component.All it dies is ti render the slides
+with the images. The Props I have used here is passed as Destructured Object.*/}
 const CorouselWrapper = ({imgs})=>{
-
+        
     return(
     <div className="carousel-inner">
         {
             imgs.map((images,i)=>(
-                <div className={`item ${i==0?'active':'' }`} >
+                <div className={`item my-container ${i==0?'active':'' }`} >
                      <img src={images} alt="Not Available"/>
                      <div className="carousel-caption">
                         <h3>SmartCow Challenge</h3>
@@ -32,7 +35,14 @@ const CorouselWrapper = ({imgs})=>{
     )
 }
 
-const Corousel = () => {
+{/* This is the main Component for the Corousel which renders the Corousel Texts, Slides and the Indicators.
+I have used states here for the Images which are being passed as props to the Functional Component.*/}
+
+{/* To achieve the text-image constrast problem, the idea I have is to change the Opacity of the Image ( which I have set to 0.5), so 
+to make the Text more readable everytime.*/}
+
+const Corousel = () => 
+{
     const [imgs] = useState(['../../assets/imgs/img1.jpg','../../assets/imgs/img2.jpg','../../assets/imgs/img3.jpg','../../assets/imgs/img4.jpg','../../assets/imgs/img5.jpg','../../assets/imgs/img6.jpg','../../assets/imgs/img7.jpg','../../assets/imgs/img8.jpg','../../assets/imgs/img9.jpg','../../assets/imgs/img10.jpg']);
     return ( 
         <div className="container">
